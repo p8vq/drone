1b319642fd97bf9457944cfd581fdda49d94f03d
p8vq <000260052@student.baisd.net>
2025-04-09 12:37:09 -0500

    Commit

---
+++ camera.py
@@ -0,0 +1,57 @@
+# This script turns on and shows the drone camera
+import cv2
+from djitellopy import tello
+from time import sleep
+import threading
+
+# Initialize drone
+drone = tello.Tello()
+
+def show_camera_frames():
+    # Shows the camera frames through cv2
+    while True:
+        frame = drone.get_frame_read().frame
+        cv2.imshow("Frame", frame)
+        # While it is running, cv2 gets the current frame on the camera and outputs it to a window
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+def movement():
+    sleep(4)
+    drone.move_up(30)
+    sleep(7)
+    drone.move_down(30)
+    sleep(7)
+    # moves up then down to see if the camera can handle it
+    drone.land()
+    # lands
+
+
+def main():
+    # Connect to drone
+    drone.connect()
+    # Turn on drone cam so we can get frames
+    sleep(1)
+    drone.takeoff()
+    sleep(3)
+    # Takes off
+    drone.streamon()
+    # Call the cv camera function thread so we actually see the drone cam
+    t1 = threading.Thread(target=show_camera_frames, args=())
+    # Makes the drone move so its not just a still image
+    t2 = threading.Thread(target=movement, args=())
+ 
+    t1.start()
+    t2.start()
+    # Starts the threads
+ 
+    t1.join()
+    t2.join()
+    
+    drone.streamoff()
+    drone.end()
+
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,27 +1,56 @@
 # This script is a long distance test
+import cv2
 from djitellopy import tello
-from time import sleep
+import keyboard
+import time
+import threading
 
 drone = tello.Tello()
 
+def show_camera_frames():
+    # Shows the camera frames through cv2
+    while True:
+        frame = drone.get_frame_read().frame
+        cv2.imshow("Frame", frame)
+        # While it is running, cv2 gets the current frame on the camera and outputs it to a window
+        if cv2.waitKey(1) & 0xFF == ord('c'):
+            break
+    cv2.destroyAllWindows()
+    
+t1 = threading.Thread(target=show_camera_frames, args=())
+t1.start()
+
 drone.connect()
-sleep(1)
 # connects and sleeps to ensure it is ready
-sleep(1)
 drone.takeoff()
-sleep(4)
+time.sleep(2)
 # takes off
 # connects and takes off
-drone.move_forward(400)
-sleep(6)
-drone.rotate_clockwise(180)
-sleep(5)
-drone.move_forward(400)
-sleep(6)
-drone.rotate_clockwise(180)
-sleep(5)
-# moves forward and turns around twice.
-# It sleeps in between each so the drone doesnt error
-drone.land()
-# lands
-drone.end()
+while True:
+    if keyboard.is_pressed('w'):
+            drone.move_forward(30)
+            time.sleep(0.6)
+            
+    if keyboard.is_pressed('s'):
+            drone.move_back(30)
+            time.sleep(0.6)
+            
+    if keyboard.is_pressed('a'):
+            drone.move_left(30)
+            time.sleep(0.6)   
+            
+    if keyboard.is_pressed('d'):
+            drone.move_right(30)
+            time.sleep(0.6)      
+            
+    if keyboard.is_pressed('q'):
+            drone.rotate_counter_clockwise(30)
+            time.sleep(0.6)             
+        
+    if keyboard.is_pressed('e'):
+            drone.rotate_clockwise(30)
+            time.sleep(0.6) 
+               
+    if keyboard.is_pressed('l'):
+            drone.land()
+            break
