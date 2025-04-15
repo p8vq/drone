d1e6ee30b0b2bf000d3d717e31307a814a38afe1
Halo3Rats <restartytoval@gmail.com>
2025-04-15 14:37:21 -0400

    new_movement

--- test.py
+++ test.py
@@ -53,7 +53,7 @@ def show_camera_frames():
         average_color_per_row = np.average(frame, axis=0)
         average_color = np.average(average_color_per_row, axis=0)
         average_color = np.uint8(average_color)
-        print(f"Average color (BGR): {average_color}, Detected: {color_name}")
+        #print(f"Average color (BGR): {average_color}, Detected: {color_name}")
 
         # Show text overlay
         cv2.putText(frame, f"Detected: {color_name}", (30, 50),
@@ -79,40 +79,56 @@ time.sleep(2)
 
 # connects and takes off
 
+aa = 0
+bb = 0 
+cc = 0
+dd = 0
+
+flipping = False
+
 while True:
-    if keyboard.is_pressed('w'):
-            drone.move_forward(30)
-            time.sleep(0.6)
-            
-    if keyboard.is_pressed('s'):
-            drone.move_back(30)
-            time.sleep(0.6)
-            
-    if keyboard.is_pressed('a'):
-            drone.move_left(30)
-            time.sleep(0.6)   
-            
-    if keyboard.is_pressed('d'):
-            drone.move_right(30)
-            time.sleep(0.6)      
-            
-    if keyboard.is_pressed('q'):
-            drone.rotate_counter_clockwise(160)
-            time.sleep(0.6)             
+      
+       drone.send_rc_control(aa, bb, cc, dd)
+      
+      
+      
+       if keyboard.is_pressed("w") and not keyboard.is_pressed("s"):
+        bb = 50
+       elif keyboard.is_pressed("s") and not keyboard.is_pressed("w"):
+        bb = -50
+       else:
+        bb = 0
+      
+       if keyboard.is_pressed("q") and not keyboard.is_pressed("e"):
+        cc = -50
+       elif keyboard.is_pressed("e") and not keyboard.is_pressed("q"):
+        cc = 50
+       else:
+        cc = 0
+       
+       if keyboard.is_pressed("a") and not keyboard.is_pressed("d"):
+        aa = -50
+       elif keyboard.is_pressed("d") and not keyboard.is_pressed("a"):
+        aa = 50
+       else:
+        aa = 0
+       
+       if keyboard.is_pressed("left") and not keyboard.is_pressed("right"):
+        dd = -50
+       elif keyboard.is_pressed("right") and not keyboard.is_pressed("left"):
+        dd = 50
+       else:
+        dd = 0
+       
+       if keyboard.is_pressed("f") and not flipping:
+        flipping = True
+        drone.flip("f")
+        time.sleep(3)
+        flipping = False
+       
+       if keyboard.is_pressed("x"):
+        drone.land()
+        break
         
-    if keyboard.is_pressed('e'):
-            drone.rotate_clockwise(160)
-            time.sleep(0.6) 
-            
-    if keyboard.is_pressed('up'):
-            drone.move_up(30)
-            time.sleep(0.6) 
-            
-    if keyboard.is_pressed('down'):
-            drone.move_down(30)
-            time.sleep(0.6) 
-               
-    if keyboard.is_pressed('l'):
-            drone.land()
-            drone.streamoff()
-            break
+
+        
